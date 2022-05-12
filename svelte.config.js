c0fb75202fa8664453630d5c59708d484bd51213
Imanuel Ulbricht <me@imanuel.dev>
2022-05-12 10:55:39 +0200

    Fixed page

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,10 @@ import adapter from '@sveltejs/adapter-static';
 
 const config = {
   kit: {
+    prerender: {
+      default: true,
+      enabled: true
+    },
     // hydrate the <div id="svelte"> element in src/app.html
     adapter: adapter({
       // default options are shown
