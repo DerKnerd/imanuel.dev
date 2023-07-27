b8e4b055ea61550a08cb9c995572402a24026d57
Imanuel Ulbricht <me@imanuel.dev>
2023-07-27 02:03:49 +0200

    Fix code style issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,10 +6,10 @@ const config = {
     // hydrate the <div id="svelte"> element in src/app.html
     adapter: adapter({
       // default options are shown
-      pages: 'build',
-      assets: 'build',
+      pages: "build",
+      assets: "build",
       fallback: null,
-      out: 'build'
+      out: "build",
     }),
   },
 };
