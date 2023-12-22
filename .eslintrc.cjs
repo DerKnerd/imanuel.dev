6225ceea4caefd34e353e690dd0258c548388f06
imanuel-dev <me@imanuel.dev>
2023-12-22 18:12:32 +0000

    Bump dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended'],
-	plugins: ['svelte3'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	extends: ['eslint:recommended', 'plugin:svelte/prettier'],
+	plugins: ['svelte'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte/svelte' }],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2019
